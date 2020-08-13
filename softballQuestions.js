4217fde37f1497bc26bb7246aee555a3f0b2f610
basedantoni <anthony.mercado300@gmail.com>
2020-08-13 12:38:55 -0500

    finished popWord

--- softballQuestions.js
+++ softballQuestions.js
@@ -1,16 +1,39 @@
-let paragraph = `
-  I think one of the hardest things in the world for people to do is to love themselves. If
+let paragraph =
+  `I think one of the hardest things in the world for people to do is to love themselves. If
   you loved yourself you would take better care of yourself, and respect the things around
   you because you respect yourself. Even the condition of your home whether clean or
   dirty can reveal how much you love yourself. Just don't expect someone else to give
   what you neglect to give yourself which is love. That's why relationships don't work out
-  so well most times.
-`;
+  so well most times.`;
 
 let string = 'The wanderlust is ever so abundant inside.'
 
 const popWord = paragraph => {
+  // Remove punctuation and extra white spaces
+  words = paragraph.replace(/[^\w\d ']/g, '')
+    .split(' ')
+    .filter(word => word);
 
+  // Assign word as value and increment each time it occurs
+  let wordDict = {}
+  words.map(word => {
+    if(!wordDict[word]) {
+      wordDict[word] = 1;
+    } else {
+      wordDict[word] += 1
+    }
+  });
+
+  let popularWord;
+  let count = 0;
+  for(let i = 0; i < words.length; i++) {
+    if(wordDict[words[i]] > count) {
+      count = wordDict[words[i]];
+      popularWord = words[i];
+    }
+  }
+
+  return popularWord
 }
 
 // Reverse but needs to fix punctuation and capital letters
@@ -27,7 +50,7 @@ const foo = string => {
 }
 
 const longestWord = paragraph => {
-  
+
 }
 
-console.log(reverse(string))
+console.log(popWord(paragraph))
