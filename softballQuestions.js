aa7dd465ae43ac7a44e7bc56a61e233a007c20f1
basedantoni <anthony.mercado300@gmail.com>
2020-08-13 09:50:57 -0500

    initial set up

---
+++ softballQuestions.js
@@ -0,0 +1,24 @@
+let paragraph = `
+  I think one of the hardest things in the world for people to do is to love themselves. If
+  you loved yourself you would take better care of yourself, and respect the things around
+  you because you respect yourself. Even the condition of your home whether clean or
+  dirty can reveal how much you love yourself. Just don't expect someone else to give
+  what you neglect to give yourself which is love. That's why relationships don't work out
+  so well most times.
+`;
+
+const popWord = paragraph => {
+
+}
+
+const reverse = string => {
+
+}
+
+const foo = string => {
+
+}
+
+const longestWord = paragraph => {
+  
+}
